384d22f2052fc518631add5e9aedfcf7c49b8f31
Caterina Powers <caterinapowers@Caterinas-MBP.fios-router.home>
2023-01-30 14:20:40 -0500

    edited UI, connected google sheet API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component, useState } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
+import axios from "axios";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    salary: '',
+    hobby: ''
+  });
+
+  const {name, age, salary, hobby} = formData;
+
+  function handleChange(event){
+    const {name, value} = event.target;
+    setFormData(prevFormData => {
+      return {...formData, [name] : value}
+    });
+  }
+
+  function handleSubmit(event){
+    console.log(formData);
+    event.preventDefault();
+
+    axios.post('https://sheet.best/api/sheets/4f13d28b-0283-44c1-99be-6cf0871dfc2f', formData)
+    .then(response => {
+      console.log(response);
+    })
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid className="container">
+      <Header as='h2'>React Google Sheets!</Header>
+      <Form className="form" onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>Name</label>
+          <input placeholder='Enter your name' type="text" name="name" value={name} onChange={handleChange}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Age</label>
+          <input placeholder='Enter your age' type="number" name="age" value={age} onChange={handleChange}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Salary</label>
+          <input placeholder='Enter your salary' type="number" name="salary" value={salary} onChange={handleChange}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Hobby</label>
+          <input placeholder='Enter your hobby' type="text" name="hobby" value={hobby} onChange={handleChange}/>
+        </Form.Field>
+        
+        <Button color="blue" type='submit'>Submit</Button>
+      </Form>
+    </Container>
+  )
+  
 }
 
 export default App;
